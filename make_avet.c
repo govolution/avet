362863c18180da16474b9966557a43379ac04705
govolution <govolution@users.noreply.github.com>
2018-09-09 15:13:07 +0200

    small error in cmd options output

--- make_avet.c
+++ make_avet.c
@@ -96,7 +96,7 @@ int main (int argc, char **argv)
 				else if (optopt == 'u')
 					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
 				else if (optopt == 'w')
-					fprintf (stderr, "Option -%w requires an argument.\n", optopt);
+					fprintf (stderr, "Option -%c requires an argument.\n", optopt);
 				else if (isprint (optopt))
 					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
 				else
