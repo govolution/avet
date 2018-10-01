79353a478605cc56aa6c52a8dabd63bda7cdafae
Florian Saager <florian.saager@rub.de>
2018-10-01 14:08:07 +0200

    Adjusted global connect config to default virtualbox host-only-network settings

--- build/global_connect_config.sh
+++ build/global_connect_config.sh
@@ -1,4 +1,4 @@
 # definition of default lhost/lport values for all build scripts
 # change to your desired default values here, if necessary
-GLOBAL_LHOST=192.168.2.103
+GLOBAL_LHOST=192.168.56.102
 GLOBAL_LPORT=443
