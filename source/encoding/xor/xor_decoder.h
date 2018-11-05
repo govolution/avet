8e83b0c99796f1100e64c660d5affb56b955c37d
Florian Saager <florian.saager@rub.de>
2018-11-05 14:55:44 +0100

    Fixed typo

--- source/encoding/xor/xor_decoder.h
+++ source/encoding/xor/xor_decoder.h
@@ -1,8 +1,5 @@
-#include "../shellcode_utility.h"
-
-
 // XORs the ciphertext with a single-byte key and stores the result in plaintext
-void decode_shellcode(const unsigned char *ciphertext, unsigned char *plaintext, const int ciphetext_length, const unsigned char key) {
+void decode_shellcode(const unsigned char *ciphertext, unsigned char *plaintext, const int ciphertext_length, const unsigned char key) {
 	for(int i = 0; i < ciphertext_length; i++) {
 		plaintext[i] = ciphertext[i] ^ key;
 	}	
