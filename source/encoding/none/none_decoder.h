1927e524c39b5cdca4af54bae79bca04ec976745
Florian Saager <florian.saager@rub.de>
2018-11-06 15:12:43 +0100

    Added none_decoder to handle cases where no encoding/decoding is applied to the shellcode

---
+++ source/encoding/none/none_decoder.h
@@ -0,0 +1,2 @@
+// Dummy function to handle options where no encoding/decoding is applied at all
+void decode_shellcode(const unsigned char *ciphertext, unsigned char *plaintext, const int ciphertext_length, const unsigned char key) {}
