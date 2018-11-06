a210d6180d9008ca3a11f39b817c306461b132aa
Florian Saager <florian.saager@rub.de>
2018-11-06 15:35:06 +0100

    Added from_command_line option for retrieving shellcode

---
+++ source/get_shellcode/from_command_line.h
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <stdlib.h>
+
+
+// Retrieves the shellcode from a "01ab23cd..." format hex string given by arg1.
+// Can be used to retrieve shellcode of such format from the command line if arg1 = argv[1].
+//
+// shellcode_size receives the size of the shellcode in bytes.
+unsigned char *get_shellcode(char *arg1, int *shellcode_size) {
+    // Get input string length  
+    int input_length = strlen(arg1);
+
+    // Convert ASCII hex string into raw bytes
+    char current_hex[5] = "0xaa";
+
+    // Each shellcode byte is represented by 2 ASCII symbols
+    *shellcode_size = input_length / 2;
+    unsigned char *shellcode = (unsigned char *) malloc(shellcode_size);
+
+    // Hex string is of format aabb01cc34...
+	// Each loop iteration handles a 2-character chunk
+    int j = 0;
+    char *endptr;						// Required by strtuol function
+
+	for(int i = 0; i < input_length; i++) {
+		// Interpret hex representation as unsigned char value 
+		memcpy(&(current_hex[2]), &(shellcode_string[2 * i]), 2);		
+		shellcode[j] = (unsigned char) strtoul(current_hex, &endptr, 16);
+		j++;
+	}   
+
+    return shellcode;     
+}
