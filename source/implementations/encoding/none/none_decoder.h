737cb76286cc662a4eec940a857441c6f0125ec0
Florian Saager <florian.saager@rub.de>
2018-11-12 13:47:19 +0100

    Fixed none-decoder to actually deliver a plaintext

--- source/implementations/encoding/none/none_decoder.h
+++ source/implementations/encoding/none/none_decoder.h
@@ -1,4 +1,8 @@
 #pragma once
 
+#include <string.h>
+
 // Dummy function to handle options where no encoding/decoding is applied at all
-void decode_none(const unsigned char *ciphertext, const int ciphertext_length, const unsigned char *key, const int key_length, unsigned char *plaintext) {}
+void decode_none(const unsigned char *ciphertext, const int ciphertext_length, const unsigned char *key, const int key_length, unsigned char *plaintext) {
+    memcpy(plaintext, ciphertext, ciphertext_length);
+}
