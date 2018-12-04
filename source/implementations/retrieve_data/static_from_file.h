f418a814cff4234a5e34f39f68d37d2354b74bb6
Florian Saager <florian.saager@rub.de>
2018-12-04 12:26:32 +0100

    Fixed missing bracket

--- source/implementations/retrieve_data/static_from_file.h
+++ source/implementations/retrieve_data/static_from_file.h
@@ -18,10 +18,11 @@ unsigned char *static_from_file(char *arg1, int *data_size) {
         DEBUG_PRINT(("Statically retrieving data from array buf[] in included file...\n"));
         *data_size = sizeof(buf) - 1;	
         return buf;
+    }
     #endif
     // Deliver address and size of array key[] if key is requested
     // If the key is retrieved statically, the define is set by the build script to notify this function that array key[] is declared and known to the compiler.
-    #ifdef STATIC_KEY    
+    #ifdef STATIC_KEY     
     if(strcmp(arg1, "static_key") == 0) {
         DEBUG_PRINT(("Statically retrieving data from array key[] in included file...\n"));
         *data_size = sizeof(key) - 1;	
