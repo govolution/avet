3b1bbf9601e2aad8678ef66d73fa55973834a57c
Florian Saager <florian.saager@rub.de>
2018-12-05 15:01:02 +0100

    Fixed data_size not being applied and returned

--- source/implementations/retrieve_data/from_command_line_raw.h
+++ source/implementations/retrieve_data/from_command_line_raw.h
@@ -13,7 +13,7 @@
 unsigned char *from_command_line_raw(char *arg1, int *data_size) {
     DEBUG_PRINT(("Retrieving data from command line arguments, expecting raw ASCII format...\n"));
     // Get input string length  
-    int input_length = strlen(arg1);
+    *data_size = strlen(arg1);
 
     // Interpret as raw data and copy into new allocated buffer
     unsigned char *data = (unsigned char *) malloc(*data_size);
