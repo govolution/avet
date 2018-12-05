2c0ab7ee6f53cc6b8d4f2f452bf24c0b625fcb7d
Florian Saager <florian.saager@rub.de>
2018-12-05 14:57:35 +0100

    Fixed function name. Fixed memcpy pointer bug.

--- source/implementations/retrieve_data/from_command_line_raw.h
+++ source/implementations/retrieve_data/from_command_line_raw.h
@@ -10,14 +10,14 @@
 // Can be used to retrieve data of such format from the command line if arg1 = argv[1].
 //
 // data_size receives the size of the data in bytes.
-unsigned char *from_command_line_hex(char *arg1, int *data_size) {
+unsigned char *from_command_line_raw(char *arg1, int *data_size) {
     DEBUG_PRINT(("Retrieving data from command line arguments, expecting raw ASCII format...\n"));
     // Get input string length  
     int input_length = strlen(arg1);
 
     // Interpret as raw data and copy into new allocated buffer
     unsigned char *data = (unsigned char *) malloc(*data_size);
-    memcpy(data, arg1, data_size);
+    memcpy(data, arg1, *data_size);
    
     return data;     
 }
