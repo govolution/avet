0ec824814c29f3cce405fae09a2e6594df45ff52
Florian Saager <florian.saager@rub.de>
2019-05-27 10:49:18 +0200

    Now uses the function argument as cpu threshold. Updated comments

--- source/implementations/evasion/get_cpu_cores.h
+++ source/implementations/evasion/get_cpu_cores.h
@@ -4,10 +4,18 @@
 #include <windows.h>
 #include "../debug_print/debug_print.h"
 
-// Argument arg1 is not used and therefore ignored.
+
+// Investigates the number of CPU cores present on the system. If core count is lower than a specified number, the program exits.
+// 
+// arg1:        Specifies the core number threshold. Program exits if core count is less than threshold.
 void get_cpu_cores(char *arg1) {
     SYSTEM_INFO sysinfo;
     GetSystemInfo(&sysinfo);
-    if (sysinfo.dwNumberOfProcessors >= MIN_CPU_CORES)
-	exit(0);
+
+    // arg1 is expected to be a valid base-10 integer representation.
+    long cpu_core_threshold = strtol(arg1, NULL, 10);
+
+    if (sysinfo.dwNumberOfProcessors < cpu_core_threshold) {
+        exit(0);
+    }
 }
