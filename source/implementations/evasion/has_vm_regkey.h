4c60112fe4f346e51a34ec170c700ac436c63d40
Florian Saager <florian.saager@rub.de>
2019-05-27 14:55:34 +0200

    Fixed search string management bug

--- source/implementations/evasion/has_vm_regkey.h
+++ source/implementations/evasion/has_vm_regkey.h
@@ -10,6 +10,7 @@
 //
 // arg1:        Specifies the set of strings to search for. Entries are comma-separated.
 //              Hence, individual search strings must not contain comma characters.
+//              Total search string length is currently limited to 1023.
 void has_vm_regkey(char *arg1) {
     DEBUG_PRINT("Applying has_vm_regkey evasion technique.\n");
     DEBUG_PRINT("Checking registry for search set %s...\n", arg1);
@@ -17,7 +18,8 @@ void has_vm_regkey(char *arg1) {
     HKEY hKey;
     int i;
     char szBuffer[1024];
-    char current_search_str[1024];
+    char complete_search_str[1024];
+    char *current_search_str;
     char *delim;      
 
     DWORD dwSize = sizeof(szBuffer) - 1;
@@ -27,15 +29,18 @@ void has_vm_regkey(char *arg1) {
         RegQueryValueEx(hKey, "0", NULL, NULL, (unsigned char *)szBuffer, &dwSize);
         
         // Compare content against search strings
-        strcpy(current_search_str, arg1);
-               
+        // Work on our own copy
+        strcpy(complete_search_str, arg1);
+        // Set current marker at beginning               
+        current_search_str = complete_search_str;
+
         // Iterate until no comma delimiter found
         while((delim = strchr(current_search_str, ',')) != NULL);
             // Terminate current string at delimiter            
             *delim = '\0';
 
             // Search for current string in registry content. If found, exit.
-            if(strstr(szBuffer, current_search_string)) {
+            if(strstr(szBuffer, current_search_str)) {
                 RegCloseKey(hKey);
                 exit(0);
             }
@@ -45,7 +50,7 @@ void has_vm_regkey(char *arg1) {
         }
 
         // Process last entry
-        if(strstr(szBuffer, current_search_string)) {
+        if(strstr(szBuffer, current_search_str)) {
             RegCloseKey(hKey);
             exit(0);
         }
