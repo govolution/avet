98d940c2331551c62c10c8b0a418dc010c1d6a2e
Thund3rPat <patricksimuttis@gmail.com>
2019-12-02 15:20:37 +0100

    Little enhancement. Fixed summary

--- build_script_tester.py
+++ build_script_tester.py
@@ -16,28 +16,42 @@ if not os.path.isdir("./testing"):
 open("./testing/result.txt", 'w').close()
 
 
+summary = "-----------------------Summary-----------------------\n"
+
+
 # run all build scripts and save output in buffer
 buffer = []
 current = 1
 for script in build_scripts:
     print("\n[%d/%d]\t--Testing %s--" % (current, count, script[8:]))
-    output = subprocess.run([script], stdout=subprocess.PIPE)
-    buffer.append("--Testing  %s--\n%s" % (script[8:], output.stdout.decode("utf-8")))
+    process = subprocess.run([script], stdout=subprocess.PIPE)
+    output = process.stdout.decode("utf-8")
+
+
+    buffer.append("--Testing  %s--\n%s" % (script[8:], output))
     
+
     current_script = script[14:-3]
+    
+
+    # todo: test for errors while building
+    # check output
+
+
     # test if .exe is generated
-    summary = "-------Summary-------\n"
     if os.path.isfile("./output/%s.exe" % current_script):
-        summary += "%s executable could be created\n" % current_script
         print("%s.exe could be created\n" % current_script)
+        summary += "%s executable could be created\n" % current_script
     else:
-        summary += "%s executable could NOT be created\n" % current_script
         print("%s.exe could NOT be created\n" % current_script)
+        summary += "%s executable could NOT be created\n" % current_script
     current += 1
 
-print(summary)
 
+print(summary)
 buffer.append(summary)
+
+
 # write buffer in file result.txt
 with open("./testing/result.txt", 'w') as result:
     result.writelines(buffer)
