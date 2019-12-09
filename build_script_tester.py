1409cd1be6d69c61d697268fbf3767e8e8e4161e
Thund3rPat <patricksimuttis@gmail.com>
2019-12-09 12:34:30 +0100

    Enhanced summary

--- build_script_tester.py
+++ build_script_tester.py
@@ -3,6 +3,7 @@
 import os
 import glob
 import subprocess
+import re
 
 # get list of build scripts and create directory named testing
 build_scripts = glob.glob('./build/build_*.sh')
@@ -16,7 +17,8 @@ if not os.path.isdir("./testing"):
 open("./testing/result.txt", 'w').close()
 
 
-summary = "-----------------------Summary-----------------------\n"
+# prepare for summary
+failed = "Build Scripts with errors:\n"
 
 
 # run all build scripts and save output in buffer
@@ -24,34 +26,44 @@ buffer = []
 current = 1
 for script in build_scripts:
     print("\n[%d/%d]\t--Testing %s--" % (current, count, script[8:]))
-    process = subprocess.run([script], stdout=subprocess.PIPE)
+    process = subprocess.run([script], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     output = process.stdout.decode("utf-8")
 
 
     buffer.append("--Testing  %s--\n%s" % (script[8:], output))
-    
+
 
     current_script = script[14:-3]
-    
 
-    # todo: test for errors while building
-    # check output
-
+    # test for errors while building
+    # Search for keywords in stdout and stderr
+    keywords = ["cannot find", "fault", "unable", "error", "Error", "Unable", "No such file or directory", "warning", "undeclared"]
+    if any(x in output for x in keywords):
+        failed += "%s:\n" % current_script
+        for item in keywords:
+            finding = re.search(".*"+item+".*", output)
+            if bool(finding):
+                print(finding.group())
+                failed += "%s:\n" % finding.group()
+        
 
     # test if .exe is generated
     if os.path.isfile("./output/%s.exe" % current_script):
-        print("%s.exe could be created\n" % current_script)
-        summary += "%s executable could be created\n" % current_script
+        print("%s executable could be created\n" % current_script)
     else:
-        print("%s.exe could NOT be created\n" % current_script)
-        summary += "%s executable could NOT be created\n" % current_script
+        print("%s executable could NOT be created\n" % current_script)
+        failed += "%s executable could NOT be created\n" % current_script
     current += 1
 
 
+# print summary
+summary = "---------------------------Summary---------------------------\n"
+summary += failed
+
 print(summary)
-buffer.append(summary)
 
 
 # write buffer in file result.txt
+buffer.append(summary)
 with open("./testing/result.txt", 'w') as result:
     result.writelines(buffer)
