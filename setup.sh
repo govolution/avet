20a2ba2fa54020305e8d9fad94015d191ff10cb4
Thund3rPat <patricksimuttis@gmail.com>
2020-03-16 06:42:21 -0400

    tdm-gcc  updated to 9.2.0

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ sudo apt install wine -y
 sudo apt install wine32 -y
 
 echo "+++ Downloading and installing tdm-gcc via wine"
-wget https://downloads.sourceforge.net/project/tdm-gcc/TDM-GCC%20Installer/tdm64-gcc-5.1.0-2.exe
-wine tdm64-gcc-5.1.0-2.exe
-rm tdm64-gcc-5.1.0-2.exe
+wget https://github.com/jmeubank/tdm-gcc/releases/download/v9.2.0-tdm64-1/tdm64-gcc-9.2.0.exe
+wine tdm64-gcc-9.2.0.exe
+rm tdm64-gcc-9.2.0.exe
 echo "+++ Finished setup procedure!"
