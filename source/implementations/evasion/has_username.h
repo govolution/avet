fcfc43bb936f2cf5c00d9cde48feeb98048d0c60
Thund3rPat <patricksimuttis@gmail.com>
2020-05-27 07:48:59 -0400

    easier implementation

--- source/implementations/evasion/has_username.h
+++ source/implementations/evasion/has_username.h
@@ -1,22 +1,20 @@
 #pragma once
 
 #include <stdio.h>
-#include <windows.h>
+#include <string.h>
+#include <windows>
 #include "../debug_print/debug_print.h"
 
 void has_unsername(char *arg1) {
-    WCHAR* username[3267];
-	DWORD charCount[3267];
 
-    if (!GetUserName(username, charCount)) {
-        DEBUG_PRINT("Could not read username.\n");
-        getchar();
-    }
+    char* username[100];
+
+    username = getenv("USERNAME");
 
     DEBUG_PRINT("Applying has_username evasion technique.\n");
     DEBUG_PRINT("Exiting if given username does not match.\n");
 
-    if (!wcscmp(username, argv[1])) {
+    if (strcmp(username, arg1)) {
 		DEBUG_PRINT("Proceed!\n")
 	} else {
 		exit(0)
