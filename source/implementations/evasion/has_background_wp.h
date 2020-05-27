a0b08be75c6c7b3d02e8f4b397778975b0539eab
Thund3rPat <patricksimuttis@gmail.com>
2020-05-27 05:49:43 -0400

    add background wallpaper check

---
+++ source/implementations/evasion/has_background_wp.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "../debug_print/debug_print.h"
+
+
+
+void has_background_wp() {
+    DEBUG_PRINT("Applying has_background_wp evasion technique.\n");
+    DEBUG_PRINT("Exiting if background wallpaper is not set\n");
+
+   if( access("%appdata\\Microsoft\\Windows\\Themes\\TranscodedWallpaper", F_OK ) != -1 ) {
+       DEBUG_PRINT("Proceed!\n");
+    } else {
+    exit(0);
+    }
+}
