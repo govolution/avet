0fd02f612b7a8b64bd81a15016b5708e56c3b236
Thund3rPat <patricksimuttis@gmail.com>
2020-06-03 06:32:27 -0400

    fix path problem

--- source/implementations/evasion/has_background_wp.h
+++ source/implementations/evasion/has_background_wp.h
@@ -3,15 +3,23 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../debug_print/debug_print.h"
 
 
 
 void has_background_wp() {
+    char * appdata = getenv("APPDATA");
+
+    if (!appdata) {
+        DEBUG_PRINT("Unable to get APPDATA.\n");
+    }
+    char buffer[1024];
+    sprintf(buffer, "%s\\Microsoft\\Windows\\Themes\\TranscodedWallpaper", appdata);
+
     DEBUG_PRINT("Applying has_background_wp evasion technique.\n");
     DEBUG_PRINT("Exiting if background wallpaper is not set\n");
-
-   if( access("%APPDATA%\\Microsoft\\Windows\\Themes\\TranscodedWallpaper", F_OK ) != -1 ) {
+   if( access(buffer, F_OK ) != -1 ) {
        DEBUG_PRINT("Proceed!\n");
     } else {
         exit(0);
