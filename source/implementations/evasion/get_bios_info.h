ef5c5e63f293cf1fe2e7a779699dcfc7ff7e6ffa
Thund3rPat <patricksimuttis@gmail.com>
2020-06-10 07:42:03 -0400

    new implementation

--- source/implementations/evasion/get_bios_info.h
+++ source/implementations/evasion/get_bios_info.h
@@ -5,21 +5,38 @@
 #include <stdlib.h>
 #include "../debug_print/debug_print.h"
 
-
+struct RawSMBIOSData
+{
+    BYTE    Used20CallingMethod;
+    BYTE    SMBIOSMajorVersion;
+    BYTE    SMBIOSMinorVersion;
+    BYTE    DmiRevision;
+    DWORD    Length;
+    BYTE    SMBIOSTableData[];
+};
 
 void get_bios_info() {
-    DWORD FirmwareTableProviderSignature, FirmwareTableID, BufferSize;
-    PVOID pFirmwareTableBuffer;
+  DWORD smBiosDataSize = 0;
+  struct RawSMBIOSData *smBiosData = NULL;
+  DWORD bytesWritten = 0;
 
-    
+  DEBUG_PRINT("Applying get_bios_info evasion technique.\n");
+  DEBUG_PRINT("Exiting if unable to fetch bios info\n");
 
-    DEBUG_PRINT("Applying get_bios_info evasion technique.\n");
-    DEBUG_PRINT("Exiting if unable to fetch bios info");
+  // Query size of SMBIOS data.
+  smBiosDataSize = GetSystemFirmwareTable('RSMB', 0, NULL, 0);
 
-    if (BufferSize <= GetSystemFirmwareTable(FirmwareTableProviderSignature, FirmwareTableID, pFirmwareTableBuffer, BufferSize)) {
-        DEBUG_PRINT("Print Bios Info:\n%s\n%s\n", FirmwareTableProviderSignature, FirmwareTableID);
-        DEBUG_PRINT("Proceed!\n");
-    } else {
-        exit(0);
-    }
+  smBiosData = (struct RawSMBIOSData*) HeapAlloc(GetProcessHeap(), 0, smBiosDataSize);
+  if (!smBiosData) {
+    DEBUG_PRINT("Out of memory\n");
+  }
+
+  // Retrieve the SMBIOS table
+  bytesWritten = GetSystemFirmwareTable('RSMB', 0, smBiosData, smBiosDataSize);
+
+  if (bytesWritten == smBiosDataSize) {
+    DEBUG_PRINT("Proceed!\n");
+  } else {
+    exit(0);
+  }
 }
