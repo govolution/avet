d0634c0176cdcf1e87b2f00e87a2d664ed8c4425
Thund3rPat <patricksimuttis@gmail.com>
2023-01-03 19:56:30 +0100

    add build scripts used for evaluation

---
+++ build/build_eval_adversarial_si.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# print AVET logo
+cat banner.txt
+
+# include script containing the compiler var $win64_compiler
+# you can edit the compiler in build/global_win64.sh
+# or enter $win64_compiler="mycompiler" here
+. build/global_win64.sh
+
+# import feature construction interface
+. build/feature_construction.sh
+
+
+
+FILES="input/malware-filtered2/*"
+N=3
+practical_manipulation="section_injection"
+population_size=100
+section_size=5
+
+for f in $FILES
+do
+    ((i=i%N)); ((i++==0)) && wait
+
+    ( cp $f output/
+    
+    # generate adversarial example
+    $genetic_optimizer -ss $section_size -pm $practical_manipulation -p $population_size output/$(basename "$f")
+
+    # cleanup
+    rm output/$(basename "$f")
+    # cleanup_techniques 
+    ) &
+done
+
+wait
